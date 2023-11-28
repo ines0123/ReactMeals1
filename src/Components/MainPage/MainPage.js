37d058445e9dfcfb6001f146f70433471b3b7f6d
ines0123 <sametines@insat.u-carthage.tn>
2023-11-28 01:14:33 +0100

    Add the NavBar to the MainPage

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,8 +1,16 @@
+import Description from "../Description/Description";
 import "./MainPage.css";
+import Navbar from "../Navbar/Navbar";
+
 export default function MainPage() {
     return (
+        <div>
+        <Navbar />
         <div className="main-page-background">
-
+            <div className="description-container">
+                <Description />
+            </div>
+        </div>
         </div>
     );
 }
