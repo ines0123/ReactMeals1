c0e6d410c4ac07b9b5f8a122a7864a19e38655b4
Balti Mohamed Aziz <mohamedaziz.balti@insat.ucar.tn>
2023-11-28 13:26:58 +0100

    popup fix part 2

--- src/Components/PopUp/PopUp.js
+++ src/Components/PopUp/PopUp.js
@@ -1,21 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext} from "react";
 import "./PopUp.css";
 import { FoodContext } from "../../Contexts/FoodContext";
 import FoodItemCart from "./FoodItemCart";
 function Popup() {
   const foods = useContext(FoodContext).foodItems;
   const setFoods = useContext(FoodContext).setFoodItems;
-  const Addamount = (food) => {
+    const Addamount = (food) => {
     const index = foods.indexOf(food);
     const copyfoods = [...foods];
     copyfoods[index].amount = parseInt(copyfoods[index].amount) + 1;
-    setFoods(copyfoods.filter((food) => food.amount > 0));
+    setFoods(copyfoods);
   };
   const Subamount = (food) => {
     const index = foods.indexOf(food);
     const copyfoods = [...foods];
     copyfoods[index].amount = parseInt(copyfoods[index].amount) - 1;
-    setFoods(copyfoods.filter((food) => food.amount > 0));
+    setFoods(copyfoods);
   };
   const totalprice = () =>
   {
@@ -23,11 +23,13 @@ function Popup() {
     foods.map((food) => (sum += food.amount*food.price));
     return sum;
   }
+  // 3 numbers after decimal point
+  const finaltotalprice = totalprice().toFixed(3);
   return (
     <>
       <div className="popup">
         <div className="popup_inner">
-          <h1 className="popup_inneritem">Cart</h1>
+          <h1 className="popup_inneritem" style={{textAlign : "center"}}>Cart</h1>
           <ul className="popup_inneritem">
             {foods.map((food) => (
               food.amount > 0 &&  
@@ -42,8 +44,8 @@ function Popup() {
               </>
             ))}
           </ul>
-          <h2 className="popup_inneritem">Total : {totalprice()}</h2>
-          <input type="button" value="Checkout" className="popup_inneritem" />
+          <h2 className="foodprice" style={{textAlign : "center"}}>Total : {finaltotalprice}$</h2>
+          <input type="button" value="Checkout" className="foodbutton" />
         </div>
       </div>
     </>
